b9afed7fa602d9164c64608ee71dde6f7df7d1eb
Moritz Schneider <moritz@Moritzs-MacBook-Pro.local>
2020-03-15 20:05:14 +0800

    make compile again

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.scss";
-import { Graph } from "./Graph";
 import { CustomGraph } from "./CustomGraph";
 import { ParentSize } from "@vx/responsive";
 import { useWindowSize } from "./useWindowSize";
